e1e2d2c4fc3d920d382d25793dc3fa0a1c44a6a5
jaewook <kjoop@naver.com>
2024-06-19 08:49:45 +0900

    6/19 SDF 파일 수정 - 조인트 결합 문제 수정

--- agv_description/launch/demo.launch.py
+++ agv_description/launch/demo.launch.py
@@ -63,10 +63,10 @@ def generate_launch_description():
             ('/tf_static', 'tf_static')
         ]
     )
-    # rviz_display_config_file = os.path.join(
-    #     get_package_share_directory('agv_description'),
-    #     'rviz',
-    #     'agv_demo.rviz')
+    rviz_display_config_file = os.path.join(
+        get_package_share_directory('agv_description'),
+        'rviz',
+        'agv_demo.rviz')
 
     joint_state_publisher = Node(
         package='joint_state_publisher',
@@ -80,11 +80,11 @@ def generate_launch_description():
         ]
     )
 
-    # rviz2 = Node(
-    #     package='rviz2',
-    #     executable='rviz2',
-    #     arguments=['-d', rviz_display_config_file],
-    #     output='screen')
+    rviz2 = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', rviz_display_config_file],
+        output='screen')
 
     # world_tf_publisher = Node(
     #     package='tf2_ros',
@@ -99,7 +99,7 @@ def generate_launch_description():
     # Add nodes to LaunchDescription
     ld.add_action(robot_state_publisher)
     ld.add_action(joint_state_publisher)
-    # ld.add_action(rviz2)
+    ld.add_action(rviz2)
     # ld.add_action(world_tf_publisher)
 
     return ld
