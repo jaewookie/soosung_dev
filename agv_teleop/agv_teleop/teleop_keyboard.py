f58c91afb49dc873e4d0329b2ff1e986d8e33675
jaewook <kjoop@naver.com>
2024-06-19 15:45:16 +0900

    원격 조종을 위한 기초 작업

---
+++ agv_teleop/agv_teleop/teleop_keyboard.py
@@ -0,0 +1,117 @@
+import os
+import select
+import sys
+import rclpy
+
+from geometry_msgs.msg import Twist
+from rclpy.qos import QoSProfile
+
+if os.name == 'nt':
+    import msvcrt
+else:
+    import termios
+    import tty
+
+LIN_VEL = 0.5
+ANG_VEL = 0.2
+
+msg = """
+---------------------------
+w : 전진
+x : 후진
+space key, s : 정지
+
+CTRL-C : 종료
+---------------------------
+"""
+
+e = """
+Communications Failed
+"""
+
+
+def get_key(settings):
+    if os.name == 'nt':
+        return msvcrt.getch().decode('utf-8')
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+    if rlist:
+        key = sys.stdin.read(1)
+    else:
+        key = ''
+
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+def main():
+    settings = None
+    if os.name != 'nt':
+        settings = termios.tcgetattr(sys.stdin)
+
+    rclpy.init()
+
+    qos = QoSProfile(depth=10)
+    node = rclpy.create_node('teleop_keyboard')
+    pub = node.create_publisher(Twist, 'cmd_vel', qos)
+
+    status = 0
+    target_linear_velocity = 0.0
+    target_angular_velocity = 0.0
+
+    try:
+        print(msg)
+        while(1):
+            key = get_key(settings)
+            if key == 'w':
+                target_linear_velocity = LIN_VEL
+                status = status + 1
+                print('전진!!!')
+            elif key == 'x':
+                target_linear_velocity = -LIN_VEL
+                status = status + 1
+                print('후진!!!')
+            elif key == ' ' or key == 's':
+                target_linear_velocity = 0.0
+                target_angular_velocity = 0.0
+                print('정지!!!')
+            else:
+                if (key == '\x03'):
+                    break
+
+            if status == 20:
+                print(msg)
+                status = 0
+
+            twist = Twist()
+
+            twist.linear.x = target_linear_velocity
+            twist.linear.y = 0.0
+            twist.linear.z = 0.0
+
+            twist.angular.x = 0.0
+            twist.angular.y = 0.0
+            twist.angular.z = 0.0
+
+            pub.publish(twist)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        twist = Twist()
+        twist.linear.x = 0.0
+        twist.linear.y = 0.0
+        twist.linear.z = 0.0
+
+        twist.angular.x = 0.0
+        twist.angular.y = 0.0
+        twist.angular.z = 0.0
+
+        pub.publish(twist)
+
+        if os.name != 'nt':
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+
+
+if __name__ == '__main__':
+    main()
